e4a69f59c705de18473c0d2f044f6e0346666197
Hironobu Nishikokura <nishiko@mindia.jp>
2015-12-30 14:39:26 +0900

    β版のAPI経由でアプリ追加とデプロイをできるようにした

--- lib/kintone/command/app.rb
+++ lib/kintone/command/app.rb
@@ -8,4 +8,16 @@ class Kintone::Command::App < Kintone::Command
   def get(id)
     @api.get(@url, id: id)
   end
+
+  def register(name)
+    url = '/k/v1/preview/app.json'
+    res = @api.post(url, name:name)
+    app = res['app'].to_i
+    deploy(app)
+  end
+
+  def deploy(app)
+    url = '/k/v1/preview/app/deploy.json'
+    @api.post(url, apps:[{app: app}])
+  end
 end
